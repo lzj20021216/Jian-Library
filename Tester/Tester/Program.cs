c549a7a5ac252362b0bfc13907937089e20b76d3
Everett Lee <lzjinsider@outlook.com>
2018-02-21 21:31:59 +0800

    Languager
    For testing only.

---
+++ Tester/Tester/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tester
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(@"
+■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■
+■                                                                           ■
+■                                                                           ■
+■                               GC_Calculator                               ■
+■                                 v2.1 beta                                 ■
+■                                                                           ■
+■                              Powered by Jian                              ■    
+■                                                                           ■
+■                         Welcome to send feedbacks                         ■
+■                  if you find bugs or have some good ideas!                ■
+■                                                                           ■ 
+■           Feedback email: xxx@outlook.com      Thank you  ^_^             ■
+■                                                                           ■");
+            Console.WriteLine("■              1. What's new     2. How to use     3. exit                  ■");
+            Console.WriteLine(@"■                                                                           ■
+■                                                                           ■
+■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■
+                ");
+            Console.ReadKey();
+        }
+    }
+}
