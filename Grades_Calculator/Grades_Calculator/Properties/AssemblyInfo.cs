8a4423a9d7f74dac70738a5789543e764de77afd
Everett Lee <lzjinsider@outlook.com>
2018-05-10 19:53:44 +0800

    Grades_Calculator
    Pre-release Build

---
+++ Grades_Calculator/Grades_Calculator/Properties/AssemblyInfo.cs
@@ -0,0 +1,39 @@
+﻿using System.Resources;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Grades_Calculator")]
+[assembly: AssemblyDescription("This is a Console program for calculating your total scores.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Tohai Studio")]
+[assembly: AssemblyProduct("Grades_Calculator")]
+[assembly: AssemblyCopyright("Copyright ©  2018  TStudio.  All rights reserved.")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("37186517-f551-465c-8025-850b8bf8b5f0")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("2.8.5.3")]
+[assembly: AssemblyFileVersion("3.0.2.0")]
+[assembly: NeutralResourcesLanguage("zh")]
+
