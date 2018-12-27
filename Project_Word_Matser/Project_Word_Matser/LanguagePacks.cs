a51ec63f8fe071ee2d833e3ee60f5f919c93fe47
Everett Lee <lzjinsider@outlook.com>
2018-12-27 22:22:11 +0800

    WM
    A new project

---
+++ Project_Word_Matser/Project_Word_Matser/LanguagePacks.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_Word_Master
+{
+    public class LanguagePacks
+    {
+        public LanguagePacks(string choices)
+        {
+            this.Choices = choices;
+        }
+        private string[] _displayLanguagePack = new string[0];
+        public string[] DisplayLanguagePack
+        {
+            get { return _displayLanguagePack; }
+            set { _displayLanguagePack = value; }
+        }
+        private string _choices;
+        public string Choices
+        {
+            get { return _choices; }
+            set
+            {
+                switch (value)
+                {
+                    case "1":
+                    case "One":
+                    case "one":
+                        _en_US();
+                        break;
+                    case "2":
+                    case "Two":
+                    case "two":
+                        _zh_CN();
+                        break;
+                    case "3":
+                    case "Three":
+                    case "three":
+                        //ja_JP
+                        break;
+                    //etc
+                    default:
+                        _en_US();
+                        break;
+                }
+                _choices = value;
+            }
+        }
+        /// <summary>
+        /// A method of a lpk
+        /// </summary>
+        /// <returns>English(United States)</returns>
+        public string[] _en_US()
+        {
+            string[] _en_US = {
+            "■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■", //0
+            @"
+            \\\\     ////    00000     1OOO0     DDDDD               MM       MM        /\        Z Z Z
+             \\\  ^  ///    0     0    1   0     D    D              M M     M M       A  A          Z
+              \\ / \ //     0     0    1 \\      D    D              M  M   M  M      A----A       Z
+               \/   \/       00000     1  \\     DDDDD               M    M    M     A      A     Z Z Z   ", //1
+            "Word Maz(Code name: Project \"Word Master\")", //2
+            "Powered by lzj20021216", //3
+            "For testing purpose only.build.csapplicationPJ_WMlzj02 ", //4
+            "1. Login", //5
+            "2. How-to", //6
+            "3. About the developer", //7
+            "     4. Exit", //8
+            "Just input one of the numbers above or leave blank to skip\nLearn about the features: ", //9
+            "-----------------------------------------------------------------------------------------------------------------------", //10
+            };
+            return DisplayLanguagePack = _en_US;
+        }
+        /// <summary>
+        /// A method of a lpk
+        /// </summary>
+        /// <returns>Chinese(Simplified)</returns>
+        public string[] _zh_CN()
+        {
+            string[] _zh_CN = {
+            };
+            return DisplayLanguagePack = _zh_CN;
+        }
+        /// <summary>
+        /// A method to show the result
+        /// </summary>
+        /// <returns>The lpk</returns>
+        public string[] DisplayLpk()
+        {
+            return DisplayLanguagePack;
+        }
+    }
+}
---
+++ Project_Word_Matser/Project_Word_Matser/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_Word_Master
+{
+    class Program
+    {
+        public static string[] _dpLpk = new string[3000];
+        public static string[] _userInputs = new string[3000];
+        public static string[] _DP_One = new string[3000];
+        public static string[] _DP_Two = new string[3000];
+        public static string[] _displayBoardShow = new string[2];
+        public static bool _recPermission = true;
+        public static bool _showPermission = true;
+        public static int _recTimer = 0;
+        public static int _permitedTimer = 0;
+        static void Main(string[] args)
+        {
+            if (_recPermission == true)
+            {
+                Console.WriteLine("Choose your language:\n1. English\n2. Chinese (Simplified)");
+                _userInputs[0] = Console.ReadLine();
+                LanguagePacks lpks = new LanguagePacks(_userInputs[0]);
+                _dpLpk = lpks.DisplayLpk();
+                WordList wlShow = new WordList(_DP_One, _DP_Two);
+                _DP_One = wlShow.DisplayPortOne();
+                _DP_Two = wlShow.DisplayPortTwo();
+                Console.Clear();
+                UserInterface();
+            }
+            Console.ReadKey(true);
+        }
+        /// <summary>
+        /// A method to show the user's interface
+        /// </summary>
+        public static void UserInterface()
+        {
+            Console.WriteLine("\n" + _dpLpk[0] + "\n\n" + _dpLpk[1] + "\n\n\n" + "\t\t\t\t\t" + _dpLpk[2] + "\n\n" + "\t\t\t\t\t\t" + _dpLpk[3] + "\n\n" + "\t\t\t\t" + _dpLpk[4] + "\n");
+            Console.Write("\t\t\t" + _dpLpk[5] + "\t" + _dpLpk[6] + "\t" + _dpLpk[7] + "\t" + _dpLpk[8] + "\n\n\n" + _dpLpk[0] + "\n\n" + _dpLpk[9]);
+            _userInputs[1] = Console.ReadLine();
+            ShowBoard();
+        }
+        public static void ShowBoard()
+        {
+            switch (_userInputs[1])
+            {
+                case "1":
+                    break;
+                case "2":
+                    _displayBoardShow[0] = null;
+                    _displayBoardShow[1] = null;
+                    break;
+                case "3":
+                    _displayBoardShow[0] = null;
+                    _displayBoardShow[1] = null;
+                    break;
+                default:
+                    break;
+            }
+            if (_showPermission)
+            {
+                Console.WriteLine(_dpLpk[10] + "\n\n" + "{0}" + "\n\n\n" + "{1}" + "\n\n" + _dpLpk[10]);
+            }
+        }
+    }
+}
